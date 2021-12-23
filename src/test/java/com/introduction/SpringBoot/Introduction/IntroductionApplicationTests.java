d22cebc4bfb834d73c4ae1b177276f93f6495bce
JaiderB <Camiloariasxd@gmail.com>
2021-12-22 21:19:32 -0500

    All initial project files are added with Spring Boot.

---
+++ src/test/java/com/introduction/SpringBoot/Introduction/IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introduction.SpringBoot.Introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
